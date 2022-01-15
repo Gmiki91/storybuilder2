f4455af9c8a077331bcc7e81733abbe572a6bf21
Miklos Gabor <gmiki91@gmail.com>
2022-01-15 19:41:08 +0100

    qucikfix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,7 +24,7 @@ exports.login= async (req, res) => {
     const {email, password} = req.body;
     const user = await User.findOne({email}).select('+password');
     if(!user || !(await user.correctPassword(password, user.password))){
-        return res.send('bukó')
+        return res.status(403).send('bukó')
     }
 
     const token = signToken(user._id);
